65055e9cb350a95b1ebb39d01420fe124ba0c8b8
Ryan Brooks <ryanbrooksis@gmail.com>
2017-03-14 12:29:42 +0000

    tweaking output

--- main.go
+++ main.go
@@ -2,58 +2,67 @@ package main
  
 import (
   "encoding/json"
-  "net/http"
-  "os"
-  "path"
+  "log"
   "math/rand"
+  "net/http"
+  "strconv"
 )
  
 func main() {
   http.HandleFunc("/", handler)
   http.HandleFunc("/about/", about)
+  http.HandleFunc("/favicon.ico", favicon)
   http.ListenAndServe(":8080", nil)
 }
 
+func favicon(w http.ResponseWriter, r *http.Request) {
+  // NOOP
+}
+
 type NumberWangMessage struct {
-  Value int
+  Value string
   NumberWang bool
   WangerNum bool
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  //rand.Intn(len(answers))
-  number := rand.Int()
-  isNumberWang := false
-  isWangerNum := false
+  betterNumbers:= [4]string{ "twentington", "shinty-six", "filth hundred and knee", "noot" }
+
+  var number string
+  if(rand.Intn(100) > 85) {
+    number = betterNumbers[rand.Intn(len(betterNumbers))]
+  }else{
+    number = strconv.Itoa(rand.Intn(1000))
+  }
+
+  isNumberWang := rand.Intn(100) > 10
+  isWangerNum := rand.Intn(100) > 90
+
   m := NumberWangMessage{number, isNumberWang, isWangerNum}
   b, err := json.Marshal(m)
 
   if err != nil {
-      panic(err)
+    log.Println(err)
+  }else{
+    log.Println(string(b))
+    w.Write(b)
   }
-  
-  w.Write(b)
 }
  
 type AboutMessage struct {
   Message string
   Version string
-  Source string
+  License string
 }
  
 func about (w http.ResponseWriter, r *http.Request) {
-  ex, err := os.Executable()
-  if err != nil {
-     panic(err)
-  }
-  exPath := path.Dir(ex)
-
-  m := AboutMessage{"This is based on the blog post http://nordicapis.com/writing-microservices-in-go/", "0.0.1", exPath}
+  m := AboutMessage{"Based on this post: http://nordicapis.com/writing-microservices-in-go/", "0.0.1", "MIT (https://opensource.org/licenses/MIT)"}
   b, err := json.Marshal(m)
 
   if err != nil {
-      panic(err)
+    log.Println(err)
+  }else{
+    log.Println(string(b))
+    w.Write(b)
   }
- 
-  w.Write(b)
 }
