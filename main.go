868d6724752ff8b0cfca46b07e09288240a6ad7a
Ryan Brooks <ryanbrooksis@gmail.com>
2017-03-14 09:37:15 +0000

    Update about to get current filename

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
-
+ 
 import (
-    "fmt"
-    "net/http"
+  "encoding/json"
+  "fmt"
+  "net/http"
+  "os"
+  "path"
 )
-
+ 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, "Welcome, %!", r.URL.Path[1:])
 }
-
+ 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", handler)
+  http.HandleFunc("/about/", about)
+  http.ListenAndServe(":8080", nil)
+}
+ 
+type Message struct {
+  Message string
+  Version string
+  Source string
+}
+ 
+func about (w http.ResponseWriter, r *http.Request) {
+  ex, err := os.Executable()
+  if err != nil {
+     panic(err)
+  }
+  exPath := path.Dir(ex)
+
+  m := Message{"This is based on the blog post http://nordicapis.com/writing-microservices-in-go/", "0.0.1", exPath}
+  b, err := json.Marshal(m)
+
+  if err != nil {
+      panic(err)
+  }
+ 
+  w.Write(b)
 }
